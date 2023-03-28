f5188fb3a64a51d17797ebaa6d541e3ea0f30492
Amrit Thapa <realamritthapa@gmail.com>
2023-03-28 01:24:06 -0700

    addded yell command

--- tickle_me.py
+++ tickle_me.py
@@ -17,7 +17,7 @@ def generate_response(prompt):
     response= openai.Completion.create(
         model= "text-davinci-003",
         prompt= prompt,
-        max_tokens= 30,
+        max_tokens= 200,
         top_p= 1,
         temperature= 0.3,
         frequency_penalty= 0,
@@ -40,12 +40,27 @@ async def on_message(message):
         return
     
     user_message = message.content.lower()
-    if "tickleme" in user_message:
-        user_message = user_message.strip("hi tickleme")
+    if "tickleme gpt" in user_message:
+        user_message = user_message.strip("tickleme gpt")
         response = str(generate_response(user_message))
         if response:
             await message.channel.send(response)
         else:
             await message.channel.send("fuck")
+    if "tickleme yell" in user_message:
+        user_message = user_message.replace("tickleme yell", '')
+        print(user_message)
+        if "me" in user_message:
+            user_message = "eat a lumpy dick " + str(message.author.display_name)
+            if user_message:
+                await message.channel.send(user_message)
+        else:
+
+            user_message = "fuck you " + str(user_message)
+            print(user_message)
+            if user_message:
+                await message.channel.send(user_message)
+    
+
 client.run(TOKEN)
 
